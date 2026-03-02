520941414b482e926ba33477a6ea4cdfbad7b4f9
zupeirr <subeer@zupeirr.com>
2026-03-02 17:12:40 +0300

    refactor(models): add alias to model associations for clarity
    - Added 'as' aliases to User and Sale associations to specify relation names
    - Assigned aliases to FuelType associations with Pump, Sale, Inventory, and Delivery
    - Added 'as' aliases for Pump and Sale associations
    - Updated Supplier and Delivery associations to include descriptive aliases
    - Improved code readability and maintainability by clarifying relationships

--- src/models/index.js
+++ src/models/index.js
@@ -7,26 +7,26 @@ const Supplier = require('./Supplier');
 const Delivery = require('./Delivery');
 
 // Define associations
-User.hasMany(Sale, { foreignKey: 'userId' });
-Sale.belongsTo(User, { foreignKey: 'userId' });
+User.hasMany(Sale, { foreignKey: 'userId', as: 'sales' });
+Sale.belongsTo(User, { foreignKey: 'userId', as: 'user' });
 
-FuelType.hasMany(Pump, { foreignKey: 'fuelTypeId' });
-Pump.belongsTo(FuelType, { foreignKey: 'fuelTypeId' });
+FuelType.hasMany(Pump, { foreignKey: 'fuelTypeId', as: 'pumps' });
+Pump.belongsTo(FuelType, { foreignKey: 'fuelTypeId', as: 'fuelType' });
 
-FuelType.hasMany(Sale, { foreignKey: 'fuelTypeId' });
-Sale.belongsTo(FuelType, { foreignKey: 'fuelTypeId' });
+FuelType.hasMany(Sale, { foreignKey: 'fuelTypeId', as: 'sales' });
+Sale.belongsTo(FuelType, { foreignKey: 'fuelTypeId', as: 'fuelType' });
 
-Pump.hasMany(Sale, { foreignKey: 'pumpId' });
-Sale.belongsTo(Pump, { foreignKey: 'pumpId' });
+Pump.hasMany(Sale, { foreignKey: 'pumpId', as: 'sales' });
+Sale.belongsTo(Pump, { foreignKey: 'pumpId', as: 'pump' });
 
-FuelType.hasOne(Inventory, { foreignKey: 'fuelTypeId' });
-Inventory.belongsTo(FuelType, { foreignKey: 'fuelTypeId' });
+FuelType.hasOne(Inventory, { foreignKey: 'fuelTypeId', as: 'inventory' });
+Inventory.belongsTo(FuelType, { foreignKey: 'fuelTypeId', as: 'fuelType' });
 
-Supplier.hasMany(Delivery, { foreignKey: 'supplierId' });
-Delivery.belongsTo(Supplier, { foreignKey: 'supplierId' });
+Supplier.hasMany(Delivery, { foreignKey: 'supplierId', as: 'deliveries' });
+Delivery.belongsTo(Supplier, { foreignKey: 'supplierId', as: 'supplier' });
 
-FuelType.hasMany(Delivery, { foreignKey: 'fuelTypeId' });
-Delivery.belongsTo(FuelType, { foreignKey: 'fuelTypeId' });
+FuelType.hasMany(Delivery, { foreignKey: 'fuelTypeId', as: 'deliveries' });
+Delivery.belongsTo(FuelType, { foreignKey: 'fuelTypeId', as: 'fuelType' });
 
 module.exports = {
     User,
